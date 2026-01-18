bce6143d8e7c0bcfa5b005e30309416f567f3782
Ibrahim Nayir <inayir@gmail.com>
2026-01-18 10:24:15 +0300

    q1

---
+++ app/genqrcode.php
@@ -0,0 +1,7 @@
+<?php
+/*Generates QR Codes*/
+$docroot=$_SERVER['DOCUMENT_ROOT'];
+require_once $docroot.'/vendor/autoload.php';
+
+
+?>
--- app/php_functions.php
+++ app/php_functions.php
@@ -124,4 +124,19 @@ function setstate($state){
 	} 
 	return $state;
 } 
+function xarray_sort(){
+    $args = func_get_args();
+    $data = array_shift($args);
+    foreach ($args as $n => $field) {
+        if (is_string($field)) {
+            $tmp = array();
+            foreach ($data as $key => $row)
+                $tmp[$key] = $row[$field];
+            $args[$n] = $tmp;
+            }
+    }
+    $args[] = &$data;
+    call_user_func_array('array_multisort', $args);
+    return array_pop($args);
+}
 ?>
